dbbbb8afb97010d5e4f3ad076a9ce9b7d8d47376
Walber Martins <walber123k@hotmail.com>
2020-07-13 16:18:01 -0300

    Refatoraçao do medoto renderFav

--- src/main.js
+++ src/main.js
@@ -1,84 +1,75 @@
+/* eslint-disable semi */
 /* eslint-disable camelcase */
-import api, { API_KEY } from './api'
-import './styles.css'
-import notificationsImg from './assets/notification.svg'
-import logoImg from './assets/logo.svg'
-import searchImg from './assets/search.svg'
-import profileImg from './assets/profile.svg'
-import favoriteImg from './assets/favorite.svg'
-import favoriteActive from './assets/favorite-active.svg'
+import api, { API_KEY } from './api';
+import './styles.css';
+import notificationsImg from './assets/notification.svg';
+import logoImg from './assets/logo.svg';
+import searchImg from './assets/search.svg';
+import profileImg from './assets/profile.svg';
+import favoriteImg from './assets/favorite.svg';
+import favoriteActive from './assets/favorite-active.svg';
 class App {
   constructor () {
-    this.favoritos = JSON.parse(localStorage.getItem('filmes')) || []
-    this.setPopularData()
-    this.setRecentData()
-    this.movies = []
-    this.now_playing = []
-    this.appElement = document.getElementById('app')
-    this.appElement.innerHTML = this.render()
-    this.ready()
-    this.eventListener()
+    this.favoritos = JSON.parse(localStorage.getItem('filmes')) || [];
+    this.setPopularData();
+    this.setRecentData();
+    this.movies = [];
+    this.now_playing = [];
+    this.appElement = document.getElementById('app');
+    this.appElement.innerHTML = this.render();
+    this.ready();
+    this.eventListener();
   }
 
   renderPopular (data) {
-    const mainEl = document.querySelector('.main-movie')
-    mainEl.innerHTML = /* html */ `<img src="https://image.tmdb.org/t/p/w780/${data[0].backdrop_path}"/>`
-    const secEl = document.querySelector('.secondary-movies')
+    const mainEl = document.querySelector('.main-movie');
+    mainEl.innerHTML = /* html */ `<img src="https://image.tmdb.org/t/p/w780/${data[0].backdrop_path}"/>`;
+    const secEl = document.querySelector('.secondary-movies');
     secEl.innerHTML = /* html */ `
     <img src="https://image.tmdb.org/t/p/w300/${data[1].backdrop_path}"/>
-    <img src="https://image.tmdb.org/t/p/w300/${data[2].backdrop_path}"/>`
+    <img src="https://image.tmdb.org/t/p/w300/${data[2].backdrop_path}"/>`;
 
     mainEl.innerHTML += /* html */ `
       <div class="info">
         <h4>${data[0].title}</h4>
         <p>${data[0].overview}</p>
-      </div>`
+      </div>`;
   }
 
   async getData (category) {
-    let data = []
+    let data = [];
     try {
       data = await api.get(
         `/movie/${category}?api_key=${API_KEY}&language=pt-BR&page=1&region=BR`
-      )
+      );
     } catch (err) {
-      console.log('Error on request ', err)
+      console.log('Error on request ', err);
     }
-    return data.data.results || []
+    return data.data.results || [];
   }
 
   async setRecentData () {
-    this.now_playing = await this.getData('now_playing')
+    this.now_playing = await this.getData('now_playing');
   }
 
   async setPopularData () {
-    this.movies = await this.getData('popular')
+    this.movies = await this.getData('popular');
   }
 
   fetchData (data, query) {
-    let html = ''
+    let html = '';
     data.forEach((filme) => {
-      const { poster_path } = filme
-      if (poster_path !== null) {
-        let favorito = false
-        const res = []
-        this.favoritos.forEach((filme) => {
-          res.push(filme.poster)
-        })
-        const index = res.indexOf(
-        `https://image.tmdb.org/t/p/w300/${poster_path}`
-        )
-        if (
-          res.indexOf(`https://image.tmdb.org/t/p/w300/${poster_path}`) !== -1 &&
-        this.favoritos[index].active
-        ) {
-          favorito = true
-        }
-        html += this.renderMovies(poster_path, favorito)
+      let favorito = false;
+      const { poster_path } = filme;
+      if (this.favoritos.indexOf(`https://image.tmdb.org/t/p/w300/${poster_path}`) !== -1) {
+        favorito = true;
       }
-    })
-    const elementAux = document.querySelector(query)
-    elementAux.innerHTML = html
+      if (poster_path !== null) {
+        html += this.renderMovies(poster_path, favorito);
+      }
+    });
+    const elementAux = document.querySelector(query);
+    elementAux.innerHTML = html;
   }
 
   renderMovies (poster_path, favorito) {
@@ -87,86 +78,67 @@ class App {
         <img class="favorite"src=${favorito ? favoriteActive : favoriteImg} />
         <img class="poster" src="https://image.tmdb.org/t/p/w300/${poster_path}"/>
         </span>
-        `
+        `;
   }
 
   ready () {
     document.addEventListener('readystatechange', (event) => {
       if (document.readyState === 'complete') {
-        this.fetchData(this.movies, '.popular > .movies')
-        this.fetchData(this.now_playing, '.recent > .movies')
-        this.renderFav()
-        this.renderPopular(this.movies)
+        this.fetchData(this.movies, '.popular > .movies');
+        this.fetchData(this.now_playing, '.recent > .movies');
+        this.renderFav();
+        this.renderPopular(this.movies);
       }
-    })
+    });
   }
 
   renderFav () {
-    const img = []
-    const fav = this.favoritos
-    const favElement = document.querySelector('.favorites > .movies')
+    const fav = this.favoritos;
+    const favElement = document.querySelector('.favorites > .movies');
     if (fav.length > 0) {
-      fav.forEach((favorito) => {
-        if (favorito.active) {
-          img.push(favorito.poster)
-        }
-      })
-      if (img.length > 0) {
-        favElement.innerHTML = ''
-        img.forEach((filme) => {
-          favElement.innerHTML += /* html */ `
+      favElement.innerHTML = '';
+      fav.forEach((filme) => {
+        favElement.innerHTML += /* html */ `
               <span key="${filme}">
                 <img class="favorite" src=${favoriteActive} />
                 <img class="poster" src="${filme}"}/>
-              </span>`
-        })
-      } else {
-        favElement.innerHTML = /* html */ `
-            <h5 class='nenhum'>Nenhum filme adicionado aos favoritos</h5>`
-      }
+              </span>`;
+      });
     } else {
       favElement.innerHTML = /* html */ `
-            <h5 class='nenhum'>Nenhum filme adicionado aos favoritos</h5>`
+            <h5 class='nenhum'>Nenhum filme adicionado aos favoritos</h5>`;
     }
-    this.favoritos = fav
-    this.saveToStorage()
+    this.saveToStorage();
   }
 
   onclick (filme) {
     const posterFilme = filme.target.src.includes('.svg')
       ? filme.srcElement.nextElementSibling.currentSrc
-      : filme.target.src
-
-    let favoritos = this.favoritos
-    const aux = []
-    favoritos.forEach((fav) => {
-      aux.push(fav.poster)
-    })
-    if (!(aux.indexOf(posterFilme) !== -1)) {
-      favoritos = [...favoritos, { active: true, poster: posterFilme }]
+      : filme.target.src;
+    const favoritos = this.favoritos;
+    if (!(favoritos.indexOf(posterFilme) !== -1)) {
+      favoritos.push(posterFilme);
     } else {
-      favoritos[aux.indexOf(posterFilme)] = {
-        active: !favoritos[aux.indexOf(posterFilme)].active,
-        poster: posterFilme
-      }
+      favoritos.splice([favoritos.indexOf(posterFilme)], 1);
     }
-    this.favoritos = favoritos
-    this.fetchData(this.movies, '.popular > .movies')
-    this.fetchData(this.now_playing, '.recent > .movies')
-    this.renderFav()
+    this.favoritos = favoritos;
+    this.fetchData(this.movies, '.popular > .movies');
+    this.fetchData(this.now_playing, '.recent > .movies');
+    this.fetchData(this.favoritos, '.favorites > .movies');
+    this.renderFav();
   }
 
   eventListener () {
-    const filmes = document.querySelectorAll('.movies')
+    const filmes = document.querySelectorAll('.movies');
     filmes.forEach((filme) => {
       filme.addEventListener('click', (filme) => {
-        this.onclick(filme)
-      })
-    })
+        this.onclick(filme);
+      });
+    });
   }
 
   saveToStorage () {
-    localStorage.setItem('filmes', JSON.stringify(this.favoritos))
+    localStorage.setItem('filmes', JSON.stringify(this.favoritos));
   }
 
   render () {
@@ -212,9 +184,9 @@ class App {
         
      </div>
 
-    `
-    return app
+    `;
+    return app;
   }
 }
 // eslint-disable-next-line no-unused-vars
-const app = new App()
+const app = new App();
